4ad122017f9d9b41b9b8d34323bd7819fc423387
ssandrini <ssandrini@itba.edu.ar>
2020-07-12 19:42:57 -0300

    version final query.h

--- query.h
+++ query.h
@@ -1,3 +1,4 @@
 #include "lectura.h"
 
-void generarQuery(ciudadADT ciudad, size_t numero); // recibe la ciudad y el numero de query, genera el archivo de salida
+void generarQuery(ciudadADT ciudad, size_t numero); // Recibe la ciudad y el numero de query, genera el archivo de salida
+// para dicho query.
