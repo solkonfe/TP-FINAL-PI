7754fbe04306769ec7f3d96043436c124cc655e0
ssandrini <ssandrini@itba.edu.ar>
2020-07-12 19:41:20 -0300

    version final de lectura.h

--- lectura.h
+++ lectura.h
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #define MAX_LONG 1024
 #define CAMPOS_BARRIOS 2
 #define CAMPOS_ARBOLES 3
 
+// Esta funcion lee las lineas del archivo de barrios y prepara los datos para su posterior procesamiento.
 void leeBarrios(const char * archivo, ciudadADT ciudad);
+
+// Esta funcion lee las lineas del archivo de arboles y prepara los datos para su posterior procesamiento.
 void leeArboles(const char * archivo, ciudadADT ciudad, size_t flag, size_t c1, size_t c2, size_t c3);
